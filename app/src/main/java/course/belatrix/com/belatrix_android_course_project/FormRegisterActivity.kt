9fa3043cf1b27aa2bb26033db067907cf9318451
Franco Iturrizaga <fiturrizaga@belatrixsf.com>
2018-02-16 15:03:51 -0500

    Stating to consctruct FormRegisterActivity

---
+++ app/src/main/java/course/belatrix/com/belatrix_android_course_project/FormRegisterActivity.kt
@@ -0,0 +1,12 @@
+package course.belatrix.com.belatrix_android_course_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class FormRegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_register)
+    }
+}
