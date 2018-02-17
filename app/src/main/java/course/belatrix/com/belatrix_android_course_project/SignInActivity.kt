0f9be522cb09b073ae576bef02bcd6d0d46d14d1
alan <alangiovannipc@gmail.com>
2018-02-17 03:25:46 -0500

    Create sign in view - Improve styles onboarding buttons

---
+++ app/src/main/java/course/belatrix/com/belatrix_android_course_project/SignInActivity.kt
@@ -0,0 +1,12 @@
+package course.belatrix.com.belatrix_android_course_project
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SigInActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sig_in)
+    }
+}
